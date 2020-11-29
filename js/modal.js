56a9712476beb5a8864e1b86fb9404d24bc74732
Peter <exteriorbwin@gmail.com>
2020-11-29 14:20:10 +0100

    finish

---
+++ js/modal.js
@@ -0,0 +1,34 @@
+const modal = document.querySelector('.modal');
+const body = document.querySelector('body');
+const firstFocusableElement = document.querySelector('.modal__close-button');
+
+const openModalListener = () => {
+    document
+        .querySelector('.open-modal-button')
+        .addEventListener('click', openModalHandler);
+};
+
+const openModalHandler = () => {
+    modal.classList.add('modal--show', 'modal--fade-in');
+    body.classList.add('modal-opened');
+    firstFocusableElement.focus();
+};
+
+const closeModalListener = () => {
+    const elements = document.querySelectorAll('.modal__overlay, .modal button')
+    elements.forEach(element => {
+        element.addEventListener('click', closeModalHandler);
+    })
+};
+
+const closeModalHandler = () => {
+    modal.classList.add('modal--fade-out');
+    modal.classList.remove('modal--fade-in');
+    body.classList.remove('modal-opened');
+    setTimeout(() => {
+        modal.classList.remove('modal--show', 'modal--fade-out');
+    }, 300)
+}
+
+openModalListener();
+closeModalListener();
